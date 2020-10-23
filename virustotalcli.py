290047fb8ad7549bb31822213be646dac78ae516
noobaly <67201532+noobaly@users.noreply.github.com>
2020-10-23 12:15:29 +0300

    Renamed the file and edited a little

--- virustotalcli.py
+++ virustotalcli.py
@@ -1,11 +1,14 @@
+# Imports
 import webbrowser
 import requests
 import json
 import time
 import sys
 import os
-#My class file
+
+# A simple python script that calculates file hashes
 import FileHash
+# --------------------------------------------------
 
 API_KEY = '<api_key>'
 
@@ -39,6 +42,7 @@ def GetFileReport(Path_to_file):
             flagged_virus+=1
 
     print("{}/{} of the AV engines flagged this file as a virus".format(flagged_virus, engine_count))
+    
 #This function uploads link for analysis
 def UploadURL(link):
     parameters = {'apikey':str(API_KEY),'url':str(link)}
@@ -46,6 +50,7 @@ def UploadURL(link):
     upload_url = requests.post(url, data=parameters)
     upload_response = upload_url.json()
     print(upload_response['verbose_msg']+"\nMeaning that you can check the results with the associated function\n It's suggested that you wait for 5 minutes(the time it takes for the URL to be analysed)")
+
 #This function checks if a given link is malicious
 def GetURLReport(link):
     parameters = {'apikey':str(API_KEY), 'resource':str(link)}
@@ -63,26 +68,31 @@ def GetURLReport(link):
             if url_report['scans'][engines]['result'] == "unrated site":
                 bad_urls-=1
                 unrated_urls+=1
-    print("\n{}/{} of the engines flagged this url as malicious and {} engines haven't rated it yet".format(bad_urls, url_engines,unrated_urls))
+    print("\n{}/{} of the engines flagged this url as malicious and {} engines didn't respond".format(bad_urls, url_engines,unrated_urls))
+    
+    
+    
 #----------------------------------------------------------------------------------------------------------------------
+
+
 if __name__=='__main__':
 
-
+    help_message="""
+        Usage (in command line):
+        {} -r -f (filepath) -> This command will show you the scan results for the file
+        {} -u -f (filepath ) -> This command will upload the file to Virustotal to scan it
+        {} -r -url (URL) -> This command will show you the scan results for the URL
+        {} -u -url (URL) -> This command will upload the URL to Virustotal to scan it
+        Note: You might need to select the directory that contains the file before using the commands
+        Note2: When typing a file path, you need to type it inside quotes if it has spaces
+        """.format(os.path.basename(__file__),os.path.basename(__file__),os.path.basename(__file__),os.path.basename(__file__))
+    
       #To generate and present report file(get's the -r and -f)
     try:
         if sys.argv[1]=='':
             print("Use: {} -h".format(os.path.basename(__file__)))
         elif sys.argv[1]=='-h':
-            print("""
-                   Usage:
-                   {} -r -f (filepath) -> This command will show you the scan results for the file
-                   {} -u -f (filepath) -> This command will upload the file to Virustotal to scan it
-                   {} -r -url (URL) -> This command will show you the scan results for the URL
-                   {} -u -url (URL) -> This command will upload the URL to Virustotal to scan it
-                   Note: You might need to select the directory that contains the file before using the commands 
-                   Note2: When typing a file path, you need to type it inside quotes if it has spaces      
-                   """.format(os.path.basename(__file__), os.path.basename(__file__), os.path.basename(__file__),
-                              os.path.basename(__file__)))
+            print(help_message)
         elif sys.argv[1]=='-r' and sys.argv[2]=='-f':
             GetFileReport(sys.argv[3])
         elif sys.argv[1]=='-u' and sys.argv[2]=='-f':
@@ -93,22 +103,14 @@ if __name__=='__main__':
             UploadURL(sys.argv[3])
 
     except IndexError:
-        print("""
-        Usage (in command line):
-        {} -r -f (filepath) -> This command will show you the scan results for the file
-        {} -u -f (filepath ) -> This command will upload the file to Virustotal to scan it
-        {} -r -url (URL) -> This command will show you the scan results for the URL
-        {} -u -url (URL) -> This command will upload the URL to Virustotal to scan it
-        Note: You might need to select the directory that contains the file before using the commands
-        Note2: When typing a file path, you need to type it inside quotes if it has spaces
-        """.format(os.path.basename(__file__),os.path.basename(__file__),os.path.basename(__file__),os.path.basename(__file__)))
+        print(help_message)
     except KeyError:
         print("This file was not found on VirusTotal database, please use the upload function to upload this file")
     except FileNotFoundError:
-        print("File you're trying to upload for does not exist, please make sure path to your file is correct!\nIf path contains spaces in between folders use \" at the start and end of the path")
+        print("File you're trying to upload does not exist, please make sure path to your file is correct!")
     except json.decoder.JSONDecodeError:
         print("The file you're trying to upload is over 32MB, this isn't allowed. This kind of big files cannot be uploaded\nPlease upload this file manualy to www.virustotal.com")
     except requests.exceptions.ConnectionError:
-        print("Exceeded the request limit, try making another request. If that doesn't work, wait for a minute and then making another request/(oryou don't have internet connection)")
+        print("Exceeded the request limit, try making another request. If that doesn't work, wait for a minute and then make another request/You don't have internet connection")
 
 
